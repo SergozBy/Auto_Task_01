f3c8cc4ac2eb58b2188972933cbdc369dbcbd7a9
Sergey Bykhavets <sergozby@gamil.com>
2024-10-31 21:01:16 +0300

    Test Remain for 1_000

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -7,10 +7,28 @@ import ru.netology.service.CashbackHackService;
 public class CashbackHackServiceTest {
 
     @Test
-    public void shouldCountRemain_900() {
+    public void shouldCountRemainFor_900() {
         CashbackHackService Count = new CashbackHackService();
         int actual = Count.remain(900);
         int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCountRemainFor_1_000() {
+        CashbackHackService Count = new CashbackHackService();
+        int actual = Count.remain(1000);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCountRemainFor_0() {
+        CashbackHackService Count = new CashbackHackService();
+        int actual = Count.remain(0);
+        int expected = 1_000;
 
         Assert.assertEquals(actual, expected);
     }
